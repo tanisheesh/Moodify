017db4909602eaf021978dc1e60b66a0376297bb
Tanish Poddar <tanish.iaf1852005@gmail.com>
2024-06-26 20:17:37 +0530

    style: splash screen fade out timing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 const { fontFamily } = require("tailwindcss/defaultTheme")
 
 export default {
-    darkMode: ["class"],
+    darkMode: ["class", "media"],
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
